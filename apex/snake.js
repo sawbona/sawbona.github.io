937e27ac9c89ea884a607424c08929017334cca5
Jacob Lobaco <jacob.lobaco@oracle.com>
2019-09-29 22:52:33 -0700

    canvas

--- apex/snake.js
+++ apex/snake.js
@@ -5,5 +5,5 @@ require.config({
 });
 
 require(['sapex/snake/game'], function(game){
-    console.log('game: ' + game);
+    console.log(game);
 });
--- apex/snake/game.js
+++ apex/snake/game.js
@@ -1,7 +1,8 @@
 define(function(){
 
     function Game(){
-        
+        var canvas = document.getElementById('canvas');
+        console.log(canvas);
     }
 
     return new Game();
