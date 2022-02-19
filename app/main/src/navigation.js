be87d42d82c38612e67f65c7aacd6880084cc57c
Jacob Lobaco <sawbona@gmail.com>
2022-02-19 14:12:58 -0800

    Transform navigation to ES6 class

--- app/main/src/navigation.js
+++ app/main/src/navigation.js
@@ -1,16 +1,17 @@
 define(["knockout"], function (ko) {
-    function Navigation() {
-        var self = this;
-        var initialView = 'home';
-        if (window.location.hash && window.location.hash.length > 0) {
-            initialView = window.location.hash;
+    class Navigation {
+        constructor() {
+            var self = this;
+            var initialView = 'home';
+            if (window.location.hash && window.location.hash.length > 0) {
+                initialView = window.location.hash;
+            }
+            self.viewName = ko.observable(initialView);
+            self.navigate = function (path) {
+                self.viewName(path);
+                return true;
+            };
         }
-        self.viewName = ko.observable(initialView);
-        self.navigate = function (path) {
-            self.viewName(path);
-            return true;
-        };
     }
-
     return new Navigation();
 });
