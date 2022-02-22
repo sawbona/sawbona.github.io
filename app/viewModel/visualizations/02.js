563557ecf0a8fc3bead82fd0054b01ab8294fa83
Jacob Lobaco <sawbona@gmail.com>
2022-02-21 17:15:49 -0800

    Happy sin 001

--- app/viewModel/visualizations/02.js
+++ app/viewModel/visualizations/02.js
@@ -1,12 +1,27 @@
+const consoleEnabled = true;
+const log = console.log;
+console.log = (...args) => {
+    if (consoleEnabled) {
+        log(...args);
+    }
+}
+
 class Corgis {
     constructor(ko) {
         const canvas = document.getElementById('circle-canvas');
-        const gl = canvas.getContext("webgl");
-        if (gl) {
-            // Set clear color to black, fully opaque
-            gl.clearColor(0.2, 0.3, 0.4, 1.0);
-            // Clear the color buffer with specified clear color
-            gl.clear(gl.COLOR_BUFFER_BIT);
+        const c = canvas.getContext("2d");
+        if (c) {
+            const start = Date.now();
+            /**
+             * 60 fps.
+             */
+            const fps = 60;
+            setInterval(() => {
+                const currentDate = Date.now();
+                const diff = (currentDate - start);
+                // c.clearRect(0, 0, canvas.width, canvas.height);
+                this.render(diff, c, canvas.width, canvas.height);
+            }, 1 * 1000 / fps);
         }
         this.isFullscreenEnabled = ko.observable(false);
         this.fullScreenText = ko.computed(() => {
@@ -18,6 +33,22 @@ class Corgis {
         this.onFullScreen = () => {
             this.isFullscreenEnabled(!this.isFullscreenEnabled());
         };
+    }
+
+    /**
+     * Render the scene. Invoked every 60 fps.
+     * @param {number} t Elapsed time.
+     * @param {Canvas2dContext} c Canvas 2d context.
+     */
+    render(t, c, w, h) {
+        const speed = 0.5;
+        c.fillStyle = `rgb(0, 8, ${(t * speed) % 200}, ${Math.sin(t * speed)} )`;
+        c.beginPath();
+        const x = (t * speed) % w;
+        const y = (h / 2) + (Math.sin(x * 2 * Math.PI / (w / 4)) * 100);
+        c.arc(x, y, 10, 0, 2 * Math.PI);
+        c.stroke();
+        c.fill();
     }
 }
 define(['knockout'], (ko) => {
