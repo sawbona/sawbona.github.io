cbb833cd9f3881f38eaf59aadc70046c10195a99
Jacob Lobaco <sawbona@gmail.com>
2022-02-21 18:13:34 -0800

    Tweak colors

--- app/viewModel/visualizations/02.js
+++ app/viewModel/visualizations/02.js
@@ -8,7 +8,7 @@ console.log = (...args) => {
 
 class Corgis {
     constructor(ko) {
-        const canvas = document.getElementById('circle-canvas');
+        const canvas = this.getCanvas();
         const c = canvas.getContext("2d");
         if (c) {
             const start = Date.now();
@@ -16,10 +16,10 @@ class Corgis {
              * 60 fps.
              */
             const fps = 60;
+            this.setup(c, canvas.width, canvas.height);
             setInterval(() => {
                 const currentDate = Date.now();
                 const diff = (currentDate - start);
-                // c.clearRect(0, 0, canvas.width, canvas.height);
                 this.render(diff, c, canvas.width, canvas.height);
             }, 1 * 1000 / fps);
         }
@@ -32,7 +32,24 @@ class Corgis {
         }
         this.onFullScreen = () => {
             this.isFullscreenEnabled(!this.isFullscreenEnabled());
+            const elem = this.getCanvas();
+            if (elem.requestFullscreen) {
+                elem.requestFullscreen();
+            } else if (elem.webkitRequestFullscreen) { /* Safari */
+                elem.webkitRequestFullscreen();
+            } else if (elem.msRequestFullscreen) { /* IE11 */
+                elem.msRequestFullscreen();
+            }
         };
+    }
+
+    getCanvas() {
+        return document.getElementById('circle-canvas');
+    }
+
+    setup(c, w, h) {
+        // c.fillStyle = "black";
+        // c.fillRect(0, 0, w, h);
     }
 
     /**
@@ -41,13 +58,19 @@ class Corgis {
      * @param {Canvas2dContext} c Canvas 2d context.
      */
     render(t, c, w, h) {
-        const speed = 0.5;
-        c.fillStyle = `rgb(0, 8, ${(t * speed) % 200}, ${Math.sin(t * speed)} )`;
+        const speed = 0.1;
+        const colorSpeed = 0.1;
+        c.fillStyle = `rgb(13, 80, ${((t * colorSpeed) % 100) + 50}, 0.45)`;
+        // console.log(`c.fillStyle = ${c.fillStyle}`);
         c.beginPath();
         const x = (t * speed) % w;
-        const y = (h / 2) + (Math.sin(x * 2 * Math.PI / (w / 4)) * 100);
-        c.arc(x, y, 10, 0, 2 * Math.PI);
-        c.stroke();
+        if (x % 10 < 9) {
+            return;
+        }
+        const circleHeight = 10;
+        const y = (h / 2) + (Math.sin(x * 2 * Math.PI / (w / 4)) * 50) + ((Math.floor((t * speed) / w)) * circleHeight * 1.8);
+        c.arc(x, y % h, circleHeight, 0, 2 * Math.PI);
+        // c.stroke();
         c.fill();
     }
 }
