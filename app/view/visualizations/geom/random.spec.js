d1cf0bd5b58d96dd8218d543c49c789731dbb148
Jacob Lobaco <sawbona@gmail.com>
2022-05-15 16:29:31 -0700

    Fix typo

--- app/view/visualizations/geom/random.spec.js
+++ app/view/visualizations/geom/random.spec.js
@@ -1,9 +1,9 @@
-import { random } from './randomUtils.js';
+import { randoms } from './randomUtils.js';
 
 const test = [
     function t1() {
         for (let i = 0; i < 100; i++) {
-            const value = random.range(20, 40);
+            const value = randoms.range(20, 40);
             if (value < 20 || value > 40) {
                 throw Error(`invalid value found: ${value}`);
             }
